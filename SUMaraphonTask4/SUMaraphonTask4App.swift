048be76b09974bb56943d97f02d60c841f2534d5
Johanssen <johnssenn@gmail.com>
2024-12-10 21:54:29 +0500

    Initial Commit

---
+++ SUMaraphonTask4/SUMaraphonTask4App.swift
@@ -0,0 +1,17 @@
+//
+//  SUMaraphonTask4App.swift
+//  SUMaraphonTask4
+//
+//  Created by A on 10.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SUMaraphonTask4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
